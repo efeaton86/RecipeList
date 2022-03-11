7bd391464aceadc53dea0051c4b6de71131b1773
efeaton86 <50210677+efeaton86@users.noreply.github.com>
2022-03-11 09:27:16 +0000

    RecipeTabView added as app entry point.

---
+++ RecipeList/Views/RecipeTabView.swift
@@ -0,0 +1,36 @@
+//
+//  RecipeTabView.swift
+//  RecipeList
+//
+//  Created by Eric Eaton on 3/11/22.
+//
+
+import SwiftUI
+
+struct RecipeTabView: View {
+    var body: some View {
+        
+        TabView {
+            Text("Feature View")
+                .tabItem {
+                    VStack {
+                        Image(systemName: "star.fill")
+                        Text("Featured")
+                    }
+                }
+            RecipeListView()
+                .tabItem {
+                    VStack {
+                        Image(systemName: "list.bullet")
+                        Text("List")
+                    }
+                }
+        }
+    }
+}
+
+struct RecipeTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        RecipeTabView()
+    }
+}
